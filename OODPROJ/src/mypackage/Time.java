0e7c9e23b1bb7c75c539382f94f710bf6ef1a375
Robert Shanahan <robshanahan5@proton.me>
2024-11-19 12:34:24 +0000

    added payslip creation for fulltime employees

--- OODPROJ/src/mypackage/Time.java
+++ OODPROJ/src/mypackage/Time.java
@@ -1,40 +1,65 @@
 package mypackage;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import mypackage.BasicPayslip;
+import mypackage.HourlyEmployee;
 
 public class Time {
-    private static LocalDate currentTime;
-    private boolean hasScaled;
+  private static LocalDate currentTime;
+  private boolean hasScaled;
 
-    public Time() {
-        currentTime = LocalDate.now();
-        hasScaled = false;
+  public Time() {
+    currentTime = LocalDate.now();
+    hasScaled = false;
+  }
+
+  public void timeCheck() {
+    if (currentTime.getMonthValue() == 10) {
+      if (!hasScaled) {
+        // TODO: scale up every employee
+
+        hasScaled = true;
+      }
+    } else {
+      hasScaled = false;
     }
 
-    public void timeCheck() {
-        if (currentTime.getMonthValue() == 10) {
-            if (!hasScaled) {
-                // TODO: scale up every employee
-                hasScaled = true;
-            }
-        } else {
-            hasScaled = false;
+    if (currentTime.getDayOfMonth() == 25) {
+      // TODO: send payslips to every employee
+      HashMap users = Userbase.getUsersbase();
+      // FullTimePayslips
+      // HourlyPayslips
+      for (User user : Userbase.getUsersbase().values()) {
+        if (user instanceof HourlyEmployee) {
+          // no hours will be submitted so idk
+        } else if (user instanceof Employee) {
+          Employee fullTime = (Employee) user;
+          String month = currentTime.getMonth().toString();
+          String day = currentTime.getDayOfWeek().toString();
+          try {
+            BasicPayslip payslip = new BasicPayslip(fullTime.getEmployeeID(),
+                fullTime.getPassword());
+            payslip.FullTimePayslip(fullTime.getEmployeeID(), month, day,
+                currentTime.getYear());
+          } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println("could not create payslip");
+          }
         }
-
-        if (currentTime.getDayOfMonth() == 25) {
-            // TODO: send payslips to every employee
-        }
+      }
     }
+  }
 
-    // call this in main to simulate days passing
-    public void moveDays(int length) {
-        for (int i = 0; i < length; i++) {
-            currentTime.plusDays(1);
-            timeCheck();
-            // I think we should be able to check if the date was changed
-            // automatically instead of manually calling a timeCheck()
-            // If the system was running, there would be no way of it changing time
-            // other than this method being called explicitly
-        }
+  // call this in main to simulate days passing
+  public void moveDays(int length) {
+    for (int i = 0; i < length; i++) {
+      currentTime.plusDays(1);
+      timeCheck();
+      // I think we should be able to check if the date was changed
+      // automatically instead of manually calling a timeCheck()
+      // If the system was running, there would be no way of it changing time
+      // other than this method being called explicitly
     }
+  }
 }
--- OODPROJ/src/mypackage/Userbase.java
+++ OODPROJ/src/mypackage/Userbase.java
@@ -6,114 +6,120 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Userbase {
-    private static HashMap<String, User> usersbase; // Store User objects
-    private static boolean loggedin = false;
+  private static HashMap<String, User> usersbase; // Store User objects
+  private static boolean loggedin = false;
 
-    public Userbase() throws FileNotFoundException {
-        usersbase = new HashMap<>();
+  public Userbase() throws FileNotFoundException {
+    usersbase = new HashMap<>();
 
-        // Add the Admin and HumanResources users directly
-        usersbase.put("admin", new Admin("admin", "admin123"));
-        usersbase.put("hr", new HumanResources("hr", "hr123"));
+    // Add the Admin and HumanResources users directly
+    usersbase.put("admin", new Admin("admin", "admin123"));
+    usersbase.put("hr", new HumanResources("hr", "hr123"));
 
-        loadEmployees();
+    loadEmployees();
+  }
+
+  /**
+   * Login method for the system
+   *
+   * @throws FileNotFoundException
+   */
+  public static void Login() {
+    loadEmployees(); // Reload employees in case of new additions or
+                     // modifications
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\nPlease enter your username (or \"q\" to quit): ");
+    String username = sc.nextLine();
+    if (username.equals("q")) {
+      System.exit(0);
     }
+    System.out.println("Please enter your password: ");
+    String password = sc.nextLine();
 
-    /**
-     * Login method for the system
-     * 
-     * @throws FileNotFoundException
-     */
-    public static void Login() {
-        loadEmployees(); // Reload employees in case of new additions or modifications
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\nPlease enter your username (or \"q\" to quit): ");
-        String username = sc.nextLine();
-        if (username.equals("q")) {
-            System.exit(0);
-        }
-        System.out.println("Please enter your password: ");
-        String password = sc.nextLine();
-
-        try {
-            checkLogin(username);
-            checkPassword(username, password);
-            if (loggedin) {
-                // Polymorphic call to displayOptions based on user type
-                User user = usersbase.get(username);
-                user.displayOptions();
-            }
-        } catch (LoginException e) {
-            System.out.println(e.getMessage());
-            Login();
-        } catch (Exception e) {
-            // System.out.println("Invalid password. Try again.");
-            System.out.println(e);
-            Login();
-        }
-        sc.close();
-    }
-
-    /**
-     * Check if the username is valid
-     * 
-     * @param username
-     * @throws LoginException
-     */
-    public static void checkLogin(String username) throws LoginException {
-        if (usersbase.containsKey(username)) {
-            System.out.println("\nUsername is valid.");
-        } else {
-            throw new LoginException("\nUsername is not valid.");
-        }
-    }
-
-    /**
-     * Check if the password is valid
-     * 
-     * @param username
-     * @param password
-     * @throws Exception
-     */
-    public static void checkPassword(String username, String password) throws Exception {
+    try {
+      checkLogin(username);
+      checkPassword(username, password);
+      if (loggedin) {
+        // Polymorphic call to displayOptions based on user type
         User user = usersbase.get(username);
-        if (user.getPassword().equals(password)) {
-            System.out.println("Password is valid.");
-            System.out.println("Logged in successfully.");
-            loggedin = true;
-        } else {
-            throw new Exception("Incorrect password.");
-        }
+        user.displayOptions();
+      }
+    } catch (LoginException e) {
+      System.out.println(e.getMessage());
+      Login();
+    } catch (Exception e) {
+      // System.out.println("Invalid password. Try again.");
+      System.out.println(e);
+      Login();
     }
+    sc.close();
+  }
 
-    /**
-     * Load all employee records from the employee_database.csv file
-     * 
-     * @throws FileNotFoundException
-     */
-    public static void loadEmployees() {
-        // Load all employee records from the employee_database.csv file
-        try {
-            Scanner sc = new Scanner(new File("OODPROJ/src/mypackage/employee_database.csv"));
-            sc.useDelimiter("\n");
+  public static HashMap<String, User> getUsersbase() { return usersbase; }
 
-            while (sc.hasNext()) {
-                String line = sc.next().trim();
-                String[] lines = line.split(",");
-                String name = lines[0];
-                String username = lines[1];
-                String employeeID = lines[2];
-                String field = lines[3];
-                String role = lines[4]; // Specific role within Employee category
-                int scale = Integer.parseInt(lines[5]);
-
-                // Treat every entry in the CSV as an Employee
-                User employee = new Employee(name, username, employeeID, field, role, scale);
-                usersbase.put(username, employee);
-            }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
+  /**
+   * Check if the username is valid
+   *
+   * @param username
+   * @throws LoginException
+   */
+  public static void checkLogin(String username) throws LoginException {
+    if (usersbase.containsKey(username)) {
+      System.out.println("\nUsername is valid.");
+    } else {
+      throw new LoginException("\nUsername is not valid.");
     }
+  }
+
+  /**
+   * Check if the password is valid
+   *
+   * @param username
+   * @param password
+   * @throws Exception
+   */
+  public static void checkPassword(String username, String password)
+      throws Exception {
+    User user = usersbase.get(username);
+    if (user.getPassword().equals(password)) {
+      System.out.println("Password is valid.");
+      System.out.println("Logged in successfully.");
+      loggedin = true;
+    } else {
+      throw new Exception("Incorrect password.");
+    }
+  }
+
+  /**
+   * Load all employee records from the employee_database.csv file
+   *
+   * @throws FileNotFoundException
+   */
+  public static void loadEmployees() {
+    // Load all employee records from the employee_database.csv file
+    try {
+      Scanner sc =
+          new Scanner(new File("OODPROJ/src/mypackage/employee_database.csv"));
+      sc.useDelimiter("\n");
+
+      while (sc.hasNext()) {
+        String line = sc.next().trim();
+        String[] lines = line.split(",");
+        String name = lines[0];
+        String username = lines[1];
+        String employeeID = lines[2];
+        String field = lines[3];
+        String role = lines[4]; // Specific role within Employee category
+        int scale = Integer.parseInt(lines[5]);
+
+        // Treat every entry in the CSV as an Employee
+        User employee =
+            new Employee(name, username, employeeID, field, role, scale);
+        usersbase.put(username, employee);
+      }
+      sc.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+    }
+  }
 }
